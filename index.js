d771297df5237e26ad76d8b858df40cb1ece753c
Maxime Golfier <maxgolfier@gmail.com>
2020-09-25 10:57:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,40 +15,40 @@ function median(values){
 
   return (values[half - 1] + values[half]) / 2.0;
 }
-var nb_post_dernier_24h = 50000;
-var posts_24h = [5, 4, 0, 0, 0, 2, 0, 9];
-var posts_24h = [];
-for(var i = 0; i<nb_post_dernier_24h; i++) {
-  posts_24h.push(getRandomArbitrary(0,100));
+var nb_element_dernier_24h = 50000;
+var elements_24h = [5, 4, 0, 0, 0, 2, 0, 9];
+var elements_24h = [];
+for(var i = 0; i<nb_element_dernier_24h; i++) {
+  elements_24h.push(getRandomArbitrary(0,100));
 }
-//console.log(posts_24h)
+//console.log(elements_24h)
 
 var tot = 0;
-for(var i = 0; i<posts_24h.length; i++) {
-  tot += posts_24h[i];
+for(var i = 0; i<elements_24h.length; i++) {
+  tot += elements_24h[i];
 }
-var moyenne = (1 / nb_post_dernier_24h) * tot;
+var moyenne = (1 / nb_element_dernier_24h) * tot;
 console.log('MOYENNE : ',moyenne)
 
-var median = median(posts_24h);
+var median = median(elements_24h);
 console.log('MEDIAN : ',median)
 
 var ecart_type_tot = 0;
-for(var i = 0; i<posts_24h.length; i++) {
-  ecart_type_tot +=  (posts_24h[i] - moyenne) * (posts_24h[i] - moyenne);
+for(var i = 0; i<elements_24h.length; i++) {
+  ecart_type_tot +=  (elements_24h[i] - moyenne) * (elements_24h[i] - moyenne);
 }
-var ecart_type = Math.sqrt( ((1 / nb_post_dernier_24h) * ecart_type_tot) );
+var ecart_type = Math.sqrt( ((1 / nb_element_dernier_24h) * ecart_type_tot) );
 console.log('ECART TYPE : ',ecart_type)
 
 var crit = (1 * ecart_type) + median;
 console.log('CRITERE : ',crit)
 
 var selected = [];
-for(var i = 0; i<posts_24h.length; i++) {
-  if(posts_24h[i] > crit) {
-    selected.push(posts_24h[i])
+for(var i = 0; i<elements_24h.length; i++) {
+  if(elements_24h[i] > crit) {
+    selected.push(elements_24h[i])
   }
 }
 //console.log(selected)
-console.log('NOMBRE DE POSTS SELECTIONNES : ', selected.length)
-console.log('% SELECTIONNES : ', (selected.length / posts_24h.length) * 100, '%')
+console.log('NOMBRE DE ELEMENTS SELECTIONNES : ', selected.length)
+console.log('% SELECTIONNES : ', (selected.length / elements_24h.length) * 100, '%')
